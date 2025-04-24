ee5fd6971c248ccc05ea0629da2ee356aedeaacd
CJY9697 <jy970114@naver.com>
2025-04-24 15:58:27 +0900

    유저 생성 함수 구현, POST /users 라우트 추가

--- run.py
+++ run.py
@@ -2,8 +2,5 @@ from app import create_app
 
 application = create_app()
 
-for rule in application.url_map.iter_rules():
-    print(rule)
-    
 if __name__ == "__main__":
     application.run(debug=True)
