8aee652fede00b202c7e8f44ebaebd32b4a0675a
CJY9697 <jy970114@naver.com>
2025-04-24 16:11:29 +0900

    POST /signup 라우트 수정, 성공시 응답 추가

--- app/routes.py
+++ app/routes.py
@@ -4,23 +4,20 @@ from app.services.users import create_user
 # 사용자 회원가입용 블루프린트 생성
 routes = Blueprint('user_bp', __name__)
 
-@routes.route('/users', methods=['POST'])
-def create_user_route():
+@routes.route('/signup', methods=['POST'])
+def signup():
 
     data = request.get_json()
     name = data.get('name')       # 이름
     email = data.get('email')     # 이메일
-    gender = data.get('gender')   # 성별
     age = data.get('age')         # 나이
+    gender = data.get('gender')   # 성별
 
     # create_user 함수 호출로 사용자 생성
     user = create_user(name, email, gender, age)
 
-    # 생성된 사용자 정보를 JSON으로 반환
+    # 성공 시 응답 메시지와 사용자 ID 반환
     return jsonify({
-        "id": user.id,
-        "name": user.name,
-        "email": user.email,
-        "gender": user.gender,
-        "age": user.age
-    }), 201
+        "message": f"{user.name}님 회원가입을 축하합니다",
+        "user_id": user.id
+    }), 200
